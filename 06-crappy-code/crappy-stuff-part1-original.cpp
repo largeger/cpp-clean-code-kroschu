5308abb7a2105baf542b30e0924ea4b3eef70c23
Lars Gerhard <lars.gerhard@brights.com>
2024-11-08 08:21:39 +0100

    crappy-code-part1 with comments

--- 06-crappy-code/crappy-stuff-part1-original.cpp
+++ 06-crappy-code/crappy-stuff-part1-original.cpp
@@ -3,10 +3,13 @@
 #include <string>
 #include <ctime>
 
+// bad choice for class name, use a more descriptive name
 class Manage
 {
 public:
+    // encapsulating age?
     int age;
+    // bad choice for a name, remove the unnecessary comment 
     std::string nam; // This variable represents the name
 
     Manage() {
@@ -14,7 +17,8 @@ public:
         nam = "Unnamed";
     }
 
-    void set_values(int a, std::string n) {
+    //parameter names are better described as age and name
+    void set_values(int a, std::string n) { // camel-case - not following naming convention, setter for two values is not the general intention
         age = a;
         nam = n;
     }
@@ -24,34 +28,46 @@ public:
     }
 };
 
+// 1. inconsistet method names
+// 2. Control is a bad choice for a name
+// 3. Single Responsibility Principle is hurt...
+// 3.1 clarify this class' purpose
 class Control
 {
 public:
-    static void print_time() { 
+    static void print_time() { // Why is this mehtod static? 
         std::time_t t = std::time(nullptr);
         std::cout << "Time: " << std::asctime(std::localtime(&t));
     }
 
     void printStatus() { 
-        // Print the status
+        // remove comment: // Print the status
         std::cout << "Status: Active\n";
     }
 };
 
 class Worker 
 {
+    // is m_ the convention for member variables
+    // m_value does not have to be a member variable
     int m_value;
-    std::vector<int> m_data; 
+    std::vector<int> m_data; // choose more descriptive names
 public:
     Worker(int value = 0) {
         m_value = value;
     }
 
+    /*
+    1. choose a meaningful name
+    2. this method has two different jobs -> separate that!
+    3. remove the comments!
+    */
     void add_value(int val) { 
         m_value += val; // Increment m_value by the input
         m_data.push_back(val); // Adds value to the data list
     }
 
+    // name not following code conventions
     void print_data() { 
         std::cout << "Values: ";
         for (auto v : m_data) {
@@ -61,27 +77,30 @@ public:
     }
 };
 
+// unnecessary class, performTask should be moved to class Worker
+
 class DoWork 
 {
 public:
-    void performTask(Worker* worker) { 
+    void performTask(Worker* worker) { // 1. The connection to worker violates the Dependency Inversion Principle!
+                                       // 2. bad naming - again...
         for (int i = 0; i < 5; i++) {
-            worker->add_value(i); // Adds incremental values to worker
+            worker->add_value(i); // unnecessary comment :  // Adds incremental values to worker
         }
     }
 };
 
 int main() {
     Manage manager;
-    manager.set_values(25, "Alice");
+    manager.set_values(25, "Alice"); 
     manager.print();
 
     Control::print_time(); 
     Control control;
     control.printStatus();
 
-    Worker work;
-    DoWork taskDoer;
+    Worker work; // bad naming: worker would be at least a bit bett
+    DoWork taskDoer; // non-speaking name
     taskDoer.performTask(&work);
     work.print_data();
 
